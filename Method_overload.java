0e0010f3d74db2e2a31e8d23c46779606d851a6c
Ahamed <muhsinjham@gmail.com>
2025-02-06 20:20:14 +0530

    practical 1b added

---
+++ Method_overload.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class Overloaded{
+    Scanner sc = new Scanner(System.in);
+    void add(){
+        System.out.println("Enter Two number : ");
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        int z = x+y;
+        System.out.println("Sum without parameter is: "+z);
+    }
+    void add(int x){
+        System.out.println("Sum with one parameter is: "+(x+x));
+    }
+    void add(int x,int y){
+        System.out.println("Sum with two parameters is: "+(x+y));
+    }
+}
+public class Method_overload {
+
+    public static void main(String[] args) {
+        Overloaded o = new Overloaded();
+        o.add();
+        o.add(10);
+        o.add(10,20);
+    }
+}
